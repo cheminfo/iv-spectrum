6d4a5c9eb47550f3066e12a1a5236b0dffbd8bdf
Luc Patiny <luc@patiny.com>
2019-11-26 13:02:37 +0100

    fix: testcases

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,15 +1,14 @@
-import { readFileSync } from "fs";
-import { join } from "path";
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-import { fromSIV } from "..";
-import { fromJcamp } from "..";
+import { fromSIV } from '..';
+import { fromJcamp } from '..';
 
-test("Test load / save jcamp", () => {
-  let text = readFileSync(join(__dirname, "../../testFiles/test.sIv"), "utf8");
+test('Test load / save jcamp', () => {
+  let text = readFileSync(join(__dirname, '../../testFiles/test.sIv'), 'utf8');
   let spectra = fromSIV(text);
   let jcamp = spectra[0].toJcamp();
   let spectrum = fromJcamp(jcamp);
   let jcamp2 = spectrum.toJcamp();
   expect(jcamp).toBe(jcamp2);
-  console.log(jcamp2);
 });
--- src/from/__tests__/fromSIV.test.js
+++ src/from/__tests__/fromSIV.test.js
@@ -1,21 +1,21 @@
-import { readFileSync } from "fs";
-import { join } from "path";
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-import fromSIV from "../fromSIV";
+import { fromSIV } from '../fromSIV';
 
-test("fromSIV", () => {
+test('fromSIV', () => {
   let siv = readFileSync(
-    join(__dirname, "../../../testFiles/test.sIv"),
-    "utf8"
+    join(__dirname, '../../../testFiles/test.sIv'),
+    'utf8'
   );
   let spectra = fromSIV(siv);
 
   let experiments = spectra.map(spectrum => spectrum.meta.experiment);
   expect(experiments).toEqual([
-    "DarkCurrent",
-    "PhotoCurrent0",
-    "PhotoCurrent2",
-    "PhotoCurrent4"
+    'DarkCurrent',
+    'PhotoCurrent0',
+    'PhotoCurrent2',
+    'PhotoCurrent4'
   ]);
   let spectrum = spectra[0];
   expect(spectrum.data.x.length).toBe(120);
