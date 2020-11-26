5bfdc88f7dac0a064764ecc75f1a2ceacd8b2d82
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-11-26 15:07:19 +0100

    fix: jcamp test

--- src/__tests__/fromJcamp.test.ts
+++ src/__tests__/fromJcamp.test.ts
@@ -4,9 +4,12 @@ import { join } from 'path';
 import { fromJcamp } from '..';
 
 test('fromJcamp', () => {
-  let jcamp = readFileSync(join(__dirname, '../../testFiles/test.jdx'), 'utf8');
-  let analysis = fromJcamp(jcamp);
-  let spectrum = analysis.spectra[0];
+  const jcamp = readFileSync(
+    join(__dirname, '../../testFiles/test.jdx'),
+    'utf8',
+  );
+  const analysis = fromJcamp(jcamp);
+  const spectrum = analysis.spectra[0];
   expect(spectrum.variables.x.data).toHaveLength(120);
   expect(spectrum.variables.y.data).toHaveLength(120);
   expect(spectrum.meta.workingTemperature).toBe(298);
