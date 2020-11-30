f417c3a949e472cbf5112ea07de2bb6d58be1a70
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-11-30 14:24:55 +0100

    feat: add parsers from powerlab cases (#3)

---
+++ src/from/fromCVd.ts
@@ -0,0 +1,41 @@
+import { Analysis } from 'common-spectrum';
+import { ndParse } from 'ndim-parser';
+
+import { toNumber } from '../utils';
+
+function parseMeta(meta: Record<string, string>): Record<string, unknown> {
+  if (!meta) return {};
+
+  let ans: Record<string, unknown> = {};
+  for (const key in meta) {
+    const listValues = meta[key].split(',');
+    const value =
+      listValues.length === 1
+        ? toNumber(listValues[0])
+        : listValues.map((v) => toNumber(v));
+    ans[key] = value;
+  }
+
+  return ans;
+}
+
+function keyMapper(keys: string[]) {
+  return keys.map((key, index) => {
+    if (key === 'Vd') return 'x';
+    if (key === 'C') return 'y';
+    return String.fromCharCode(65 + index);
+  });
+}
+
+export function fromCVd(text: string) {
+  const { data, meta } = ndParse(text, keyMapper);
+  const parsedMeta = parseMeta(meta);
+
+  let analysis = new Analysis();
+  analysis.pushSpectrum(data, {
+    title: parsedMeta['Setup title'],
+    meta: parsedMeta,
+  });
+
+  return analysis;
+}
