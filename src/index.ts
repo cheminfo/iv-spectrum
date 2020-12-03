08b160cd50f02f190ab6daacb07731905d881121
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-12-03 11:02:40 +0100

    fix: export missing parsers

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ export {
 } from 'common-spectrum';
 
 export { fromSIV } from './from/fromSIV';
+export { fromCVd } from './from/fromCVd';
+export { fromMulChannelCap } from './from/fromMulChannelCap';
