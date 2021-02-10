21e429a4d01e996118e4f44a01104fd85f538a57
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-02-10 09:56:53 +0100

    chore: add default scale to metadata

--- src/from/b1505/index.ts
+++ src/from/b1505/index.ts
@@ -1,37 +1,37 @@
 import BaseB1505 from './BaseB1505';
 
 export function fromBreakdown(text: string) {
-  return new BaseB1505('Vd', 'Id_dens', true).parseText(text);
+  return new BaseB1505('Vd', 'Id_dens', true, 'log').parseText(text);
 }
 
 export function fromHEMTBreakdown(text: string) {
-  return new BaseB1505('Vd', 'Id_density', true).parseText(text);
+  return new BaseB1505('Vd', 'Id_density', true, 'log').parseText(text);
 }
 
 export function fromTransfer(text: string) {
-  return new BaseB1505('Vg', 'Id_dens', true).parseText(text);
+  return new BaseB1505('Vg', 'Id_dens', true, 'log').parseText(text);
 }
 
 export function fromNoffTransfer(text: string) {
-  return new BaseB1505('Vg', 'Id_density', true).parseText(text);
+  return new BaseB1505('Vg', 'Id_density', true, 'log').parseText(text);
 }
 
 export function fromOutput(text: string) {
-  return new BaseB1505('Vd', 'Id_dens', true).parseText(text);
+  return new BaseB1505('Vd', 'Id_dens', true, 'linear').parseText(text);
 }
 
 export function fromNoffOutput(text: string) {
-  return new BaseB1505('Vd', 'Id_density', true).parseText(text);
+  return new BaseB1505('Vd', 'Id_density', true, 'linear').parseText(text);
 }
 
 export function fromIV(text: string) {
-  return new BaseB1505('Vd', 'Id_dens', true).parseText(text);
+  return new BaseB1505('Vd', 'Id_dens', true, 'linear').parseText(text);
 }
 
 export function fromCapacitance(text: string) {
-  return new BaseB1505('Vd', 'C_dens', false).parseText(text);
+  return new BaseB1505('Vd', 'C_dens', false, 'linear').parseText(text);
 }
 
 export function fromMOSCapacitance(text: string) {
-  return new BaseB1505('VBias', 'C_density', true).parseText(text);
+  return new BaseB1505('VBias', 'C_density', true, 'linear').parseText(text);
 }
