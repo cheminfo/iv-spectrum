856d6a097f842385f946f021c9286b4bc616f1df
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-06-24 12:05:11 +0200

    feat: create specific function for 2636b

---
+++ src/from/2636b/__tests__/from2636b.test.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+import { from2636b } from '../index';
+
+function testFile(name: string, length: number) {
+  let csv = readFileSync(
+    join(__dirname, `../../../../testFiles/2636B/${name}`),
+    'latin1',
+  );
+  const analysis = from2636b(csv);
+  let spectrum = analysis.getXYSpectrum({ xLabel: 'Vd', yLabel: 'Id' });
+
+  expect(spectrum?.variables.x.data).toHaveLength(length);
+  expect(spectrum?.variables.x.label).toStrictEqual(
+    `Vd [${spectrum?.variables.x.units || ''}]`,
+  );
+
+  expect(spectrum?.variables.y.data).toHaveLength(length);
+  expect(spectrum?.variables.y.label).toStrictEqual(
+    `Id [${spectrum?.variables.y.units || ''}]`,
+  );
+}
+
+test('IV', () => {
+  testFile('iv.csv', 601);
+});
+
+test('Output', () => {
+  testFile('output.csv', 121);
+});
+
+test('Transfer', () => {
+  testFile('transfer.csv', 201);
+});
