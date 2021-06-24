ec4b238c8a621e0c25e7e329033a771eb21817a8
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-06-24 12:08:37 +0200

    feat: create specific function for 2636b (#43)

---
+++ src/from/2636b/index.ts
@@ -0,0 +1,56 @@
+import { Analysis } from 'common-spectrum';
+import { ndParse } from 'ndim-parser';
+
+import { appendUnits } from '../b1505/utils';
+
+function parseMeta(meta: Record<string, string>): Record<string, string> {
+  if (!meta) return {};
+
+  let ans: Record<string, string> = {};
+  for (const key in meta) {
+    const line = [key, ...meta[key].split(',')];
+    for (let index = 0; index < line.length; index += 2) {
+      if (line[index]) ans[line[index]] = line[index + 1];
+    }
+  }
+
+  return ans;
+}
+
+function keyMap(keys: string[]) {
+  return keys.map((key, index) => {
+    if (key === 'Vd') return 'x';
+    if (key === 'Id') return 'y';
+    if (key === 'Vg') return 'g';
+    if (key === 'Vs') return 's';
+    return String.fromCharCode(65 + index);
+  });
+}
+
+export function from2636b(text: string) {
+  const { data, meta } = ndParse(text, { keyMap });
+
+  const min = Object.values(data).reduce(
+    (prev, curr) => Math.min(curr.data.length, prev),
+    Infinity,
+  );
+
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      data[key].data.length = min;
+    }
+  }
+
+  let analysis = new Analysis();
+  const title = data.g
+    ? `Vg = ${data.g.data[0]}V`
+    : data.s
+    ? `Vs = ${data.s.data[0]}V`
+    : '2636b';
+  analysis.pushSpectrum(appendUnits(data), {
+    title,
+    meta: parseMeta(meta),
+  });
+
+  return analysis;
+}
--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,6 @@ export {
   fromMOSCapacitance,
 } from './from/b1505';
 export { getLabels as getLabelsB1505 } from './from/b1505/utils';
+export { from2636b } from './from/2636b';
 export { fromSIV } from './from/fromSIV';
 export { fromMulChannelCap } from './from/fromMulChannelCap';
